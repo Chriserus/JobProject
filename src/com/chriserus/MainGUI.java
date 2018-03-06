798692bdcb0dc4a5cccd805cd3a956ead63a1229
krzysztof <krzysborowiecki97@gmail.com>
2018-03-06 12:50:16 +0100

    Just some JavaFX training

---
+++ src/com/chriserus/MainGUI.java
@@ -0,0 +1,33 @@
+package com.chriserus;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class MainGUI extends Application {
+
+    Button button;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Restaurant manager");
+
+        button = new Button();
+        button.setText("Click me");
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+
+        Scene scene = new Scene(layout, 300, 250);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
