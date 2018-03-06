ad41a81475e193b62ba5bb8aa3b2cf801958607a
krzysztof <krzysborowiecki97@gmail.com>
2018-03-06 12:30:26 +0100

    First commit of this project

---
+++ src/com/chriserus/Main.java
@@ -0,0 +1,16 @@
+package com.chriserus;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
