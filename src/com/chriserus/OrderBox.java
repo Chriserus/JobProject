3114c7f0b5cdf9ff50294064d8ad475f856bbb15
krzysztof <krzysborowiecki97@gmail.com>
2018-03-20 14:04:18 +0100

    Added entire GUI to OrderBox, now need to add functionality

--- src/com/chriserus/OrderBox.java
+++ src/com/chriserus/OrderBox.java
@@ -52,23 +52,23 @@ public class OrderBox extends MenuBox{
 
         //Name
         TableColumn<ItemEntity, String> nameColO = new TableColumn<>("Name");
-        nameCol.setMinWidth(200);
-        nameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
+        nameColO.setMinWidth(200);
+        nameColO.setCellValueFactory(new PropertyValueFactory<>("name"));
 
         //Price
         TableColumn<ItemEntity, Double> priceColO = new TableColumn<>("Price");
-        priceCol.setMinWidth(200);
-        priceCol.setCellValueFactory(new PropertyValueFactory<>("price"));
+        priceColO.setMinWidth(200);
+        priceColO.setCellValueFactory(new PropertyValueFactory<>("price"));
 
         //Calories
         TableColumn<ItemEntity, Integer> caloriesColO = new TableColumn<>("Calories");
-        caloriesCol.setMinWidth(200);
-        caloriesCol.setCellValueFactory(new PropertyValueFactory<>("calories"));
+        caloriesColO.setMinWidth(200);
+        caloriesColO.setCellValueFactory(new PropertyValueFactory<>("calories"));
 
         //Vegetarian
         TableColumn<ItemEntity, Boolean> vegetarianColO = new TableColumn<>("Vegetarian");
-        vegetarianCol.setMinWidth(200);
-        vegetarianCol.setCellValueFactory(new PropertyValueFactory<>("vegetarian"));
+        vegetarianColO.setMinWidth(200);
+        vegetarianColO.setCellValueFactory(new PropertyValueFactory<>("vegetarian"));
 
         table.getColumns().addAll(nameCol,priceCol,caloriesCol,vegetarianCol);
         tableOrder.getColumns().addAll(nameColO,priceColO,caloriesColO,vegetarianColO);
@@ -78,9 +78,29 @@ public class OrderBox extends MenuBox{
 
         Button addButton = new Button("Add");
         Button removeButton = new Button("Remove");
+        Button finalizeButton = new Button("Finalize");
+
+        //Adding text fields for order
+        TextField priceSum = new TextField();
+        priceSum.setPromptText("Total");
+        priceSum.setEditable(false);
+        priceSum.setMinWidth(100);
+
+        TextField caloriesSum = new TextField();
+        caloriesSum.setPromptText("Calories");
+        caloriesSum.setEditable(false);
+        caloriesSum.setMinWidth(100);
 
         addButton.setOnAction(e->addButtonClicked());
-       removeButton.setOnAction(e->deleteButtonClicked());
+        removeButton.setOnAction(e->deleteButtonClicked());
+        finalizeButton.setOnAction(e -> finalizeButtonClicked());
+
+
+        //Layout
+        GridPane gp = new GridPane();
+        gp.setHgap(10);
+        gp.setVgap(10);
+       // gp.setPadding(new Insets(10,10,10,10));
 
         VBox vBox = new VBox();
         vBox.setSpacing(10);
@@ -89,22 +109,36 @@ public class OrderBox extends MenuBox{
 
         HBox hBox = new HBox();
         hBox.setSpacing(10);
-        hBox.getChildren().addAll(table, vBox, tableOrder);
+        hBox.setAlignment(Pos.CENTER_RIGHT);
+        hBox.setPadding(new Insets(10,10,10,10));
+        hBox.getChildren().addAll(caloriesSum, priceSum, finalizeButton);
+
+
+        GridPane.setConstraints(table, 0, 0, 1, 2);
+        GridPane.setConstraints(vBox, 1, 0);
+        GridPane.setConstraints(tableOrder, 2, 0);
+        GridPane.setConstraints(hBox,2,1);
+
+
+        gp.getChildren().addAll(table, vBox, tableOrder, hBox);
+
+        Scene scene = new Scene(gp);
+
         window.initModality(Modality.APPLICATION_MODAL);
         window.setTitle("Order creator");
-
-
-        
-        Scene scene = new Scene(hBox);
 
         window.setScene(scene);
         window.show();
     }
 
+    private static void finalizeButtonClicked() {
+
+    }
+
     private static void addButtonClicked(){
         ObservableList<ItemEntity> productSelected;
         productSelected = table.getSelectionModel().getSelectedItems();
-        tableOrder.setItems(productSelected);
+        tableOrder.getItems().addAll(productSelected);
     }
 
     private static void deleteButtonClicked(){
